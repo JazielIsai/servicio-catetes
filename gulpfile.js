46a405243c3d0e77d6fcec186c3de268e34ed710
Jaziel Isai <code.isai.dev@gmail.com>
2022-01-06 11:33:43 -0600

    update style and minification css and js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const { src, dest, watch, parallel } = require('gulp');
 //CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer'); //va a segurnos que el codigo css funcione en los navegadores 
+const cssnano = require('cssnano'); //comprime el codigo de css
+const postcss = require('gulp-postcss'); // postcss hace mejoraras y transformaciones por medio de gulp-plumber, autoprefixer y cssnano
+const sourcemaps = require('gulp-sourcemaps');
 
 //Image
 const cache = require('gulp-cache');
@@ -10,11 +14,16 @@ const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 
 
+//JavaScript
+const terser = require('gulp-terser-js');
 
 function css( done ){    
     src('src/scss/**/*.scss') //Identificar el archivo .scss a compilar
+        .pipe(sourcemaps.init()) //vamos a iniciar el sourcemaps para hacer un mapeo en el codigo de css minificado para saber donde podemos modificarlo desde sass 
         .pipe( plumber() )
         .pipe( sass() )  //compilar el archivo
+        .pipe( postcss( [autoprefixer(), cssnano()] ) ) //las mejoras de css junto con la minificacion 
+        .pipe(sourcemaps.write('.')) //guardar las referencias de css en el mismo lugar/carpeta de build / css
         .pipe( dest('build/css') ) //almacenarlo en el disco duro
 
 
@@ -48,6 +57,9 @@ function versionWebp (done) {
 function javascript ( done ) {
 
     src('src/js/**/*.js')
+        .pipe( sourcemaps.init() )
+        .pipe( terser() ) //minifica javascript 
+        .pipe( sourcemaps.write('.'))
         .pipe(dest('./build/js'))
     done();
 }
